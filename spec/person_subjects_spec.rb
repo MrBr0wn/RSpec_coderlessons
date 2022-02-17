6ac8969fc1c4696469ce4b9a5a50c215c1950df8
Mr. Brown <gitcherrypick.work@gmail.com>
2022-02-17 22:06:16 +0500

    Subjects

---
+++ spec/person_subjects_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'person'
+
+describe Person do
+  it 'create a new person with a first and last name' do
+    person = Person.new 'John', 'Smith'
+
+    expect(person).to have_attributes(first_name: 'John')
+    expect(person).to have_attributes(last_name: 'Smith')
+  end
+end
+
+# Using the subject into describe block. Equal that what above
+describe Person.new 'John', 'Smith' do
+  it { is_expected.to have_attributes(first_name: 'John') }
+  it { is_expected.to have_attributes(last_name: 'Smith') }
+end
