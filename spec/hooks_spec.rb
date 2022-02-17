7aeb418799ee0ec821fd7b479b9e67a2d2b6aa8f
Mr. Brown <gitcherrypick.work@gmail.com>
2022-02-17 21:23:11 +0500

    Hooks before and after

---
+++ spec/hooks_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'hooks'
+
+describe SimpleClass do
+  before(:each) do
+    @simple_class = SimpleClass.new
+  end
+
+  it 'should have an initial message' do
+    expect(@simple_class).to_not be_nil
+    @simple_class.message = 'Something else...'
+  end
+
+  it 'should be able to change it\'s message' do
+    @simple_class.update_message('a new message')
+    expect(@simple_class.message).to_not be 'howdy'
+  end
+end
